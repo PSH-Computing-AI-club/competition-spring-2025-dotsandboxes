8834be8c3295618c8e10d1598cf4e4073dcbe146
oridim <125223080+oridim@users.noreply.github.com>
2025-01-03 22:10:44 -0500

    feat: tracking the player's player instance in the `Game` namespace

--- worker/game_namespace.ts
+++ worker/game_namespace.ts
@@ -1,24 +1,29 @@
-import type { IGameBoard, IGameSession } from '../engine/mod.ts';
+import type { IGameBoard, IGameSession, IPlayer } from '../engine/mod.ts';
 
 export interface IGameNamespaceOptions {
     readonly gameBoard: IGameBoard;
 
     readonly gameSession: IGameSession;
+
+    readonly player: IPlayer;
 }
 
 export interface IGameNamespace {
     readonly board: IGameBoard;
 
     readonly session: IGameSession;
+
+    readonly player: IPlayer;
 }
 
 export function makeGameNamespace(
     options: IGameNamespaceOptions,
 ): IGameNamespace {
-    const { gameBoard, gameSession } = options;
+    const { gameBoard, gameSession, player } = options;
 
     return Object.freeze({
         board: gameBoard,
         session: gameSession,
+        player,
     });
 }
