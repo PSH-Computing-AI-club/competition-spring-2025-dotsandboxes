f1aef3eb088d3d424df904b17b93c86f4bbc7223
oridim <125223080+oridim@users.noreply.github.com>
2025-01-04 03:32:00 -0500

    feat: ported random player to run in script environment

---
+++ players/random_player.js
@@ -0,0 +1,24 @@
+const { board: gameBoard } = globalThis.Game;
+const { SLOT_KIND } = globalThis.Engine;
+
+globalThis.onComputePlayerTurn = () => {
+    const availableSpacers = Array.from(gameBoard.walkSpacers())
+        .filter(
+            (gameBoardSlot) => gameBoardSlot.slotKind === SLOT_KIND.spacer,
+        );
+
+    const boardSlotIndex = Math.trunc(
+        (availableSpacers.length - 1) * Math.random(),
+    );
+
+    const gameBoardSlot = availableSpacers[boardSlotIndex];
+
+    if (gameBoardSlot === undefined) return null;
+
+    const { x, y } = gameBoardSlot;
+
+    return {
+        x,
+        y,
+    };
+};
