6a9ecf921f9d4f3953c9e13a22767f041514c843
oridim <125223080+oridim@users.noreply.github.com>
2025-01-04 00:16:08 -0500

    feat: added module level exports for worker sub library

---
+++ worker/mod.ts
@@ -0,0 +1,5 @@
+export * from './engine_namespace.ts';
+export * from './game_namespace.ts';
+export * from './math_namespace.ts';
+export type * from './worker.ts';
+export * from './worker_global_this.ts';
