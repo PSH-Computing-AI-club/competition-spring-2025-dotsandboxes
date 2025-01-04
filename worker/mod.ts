3fd872018db9449822eb481b8cf7cd665cc4eaa5
oridim <125223080+oridim@users.noreply.github.com>
2025-01-04 03:51:02 -0500

    feat: added player script handlers to module exports

--- worker/mod.ts
+++ worker/mod.ts
@@ -1,5 +1,6 @@
 export * from './engine_namespace.ts';
 export * from './game_namespace.ts';
 export * from './math_namespace.ts';
+export * from './player_script.ts';
 export type * from './worker.ts';
 export * from './worker_global_this.ts';
