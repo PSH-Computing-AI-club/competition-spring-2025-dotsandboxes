d1605d0b40e85275816251adba68520b0ca675fc
oridim <125223080+oridim@users.noreply.github.com>
2025-01-04 03:54:22 -0500

    feat: added initial bundling functions

--- worker/player_script.ts
+++ worker/player_script.ts
@@ -1,4 +1,5 @@
 import { createContext, Script } from 'node:vm';
+import { bundle } from 'jsr:@deno/emit';
 
 import { IPlayerMove } from '../engine/mod.ts';
 
@@ -6,12 +7,36 @@ import type { IWorkerGlobalThis } from './worker_global_this.ts';
 
 export type IComputePlayerTurnCallback = () => IPlayerMove;
 
+export interface IBundlePlayerScriptOptions {
+    readonly root: string | URL;
+}
+
 export interface IEvaluatePlayerScriptOptions {
     readonly code: string;
 
     readonly globalThis: IWorkerGlobalThis;
 
     readonly timeout: number;
+}
+
+export async function bundlePlayerScript(
+    options: IBundlePlayerScriptOptions,
+): Promise<string> {
+    const { root } = options;
+
+    const { code } = await bundle(root, {
+        allowRemote: false,
+        minify: true,
+        type: 'classic',
+
+        compilerOptions: {
+            checkJs: true,
+            inlineSourceMap: true,
+            inlineSources: true,
+        },
+    });
+
+    return code;
 }
 
 export async function evaluatePlayerScript(
