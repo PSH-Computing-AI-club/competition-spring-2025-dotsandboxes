c340756d5888ebc5473f7dd1a9e8674142b42d55
oridim <125223080+oridim@users.noreply.github.com>
2025-01-05 02:16:50 -0500

    feat: converted random player to typescript and added header comment

---
+++ players/random_player.ts
@@ -0,0 +1,35 @@
+// Random Player randomly selects from a pool of available lines to draw.
+
+const { board: gameBoard } = Game;
+const { SLOT_KIND } = Engine;
+
+export default (() => {
+    const availableSpacers = Array.from(gameBoard.walkSpacers())
+        .filter(
+            (gameBoardSlot) => {
+                const { slotKind } = gameBoardSlot;
+
+                return slotKind === SLOT_KIND.spacer;
+            },
+        );
+
+    const boardSlotIndex = Math.max(
+        Math.trunc(
+            (availableSpacers.length - 1) * Math.random(),
+        ),
+        0,
+    );
+
+    const gameBoardSlot = availableSpacers[boardSlotIndex];
+
+    if (gameBoardSlot !== undefined) {
+        const { x, y } = gameBoardSlot;
+
+        return {
+            x,
+            y,
+        };
+    }
+
+    return null;
+}) satisfies PlayerScript.IComputePlayerTurnCallback;
