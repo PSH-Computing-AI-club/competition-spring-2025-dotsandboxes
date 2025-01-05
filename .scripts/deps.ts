10a6b0c80a377190a843a7c20de00a3796fc9f8d
oridim <125223080+oridim@users.noreply.github.com>
2025-01-05 11:00:58 -0500

    chore: forgot to keep up on adding dependency import script

--- .scripts/deps.ts
+++ .scripts/deps.ts
@@ -1,6 +1,10 @@
 // We want a script file to collect all of the depenedencies we are using
 // so that Deno can easily cache them ahead-of-time rather than just-in-time.
 
+import '@cliffy/command';
+import '@deno/emit';
 import '@std/assert';
 import '@std/log';
-import '@cliffy/command';
+import '@std/path';
+import '@std/random';
+import '@workers/caplink';
