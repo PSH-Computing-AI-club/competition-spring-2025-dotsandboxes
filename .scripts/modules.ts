fc356957d1398d5d9eb25454530f1340b5cb2d07
oridim <125223080+oridim@users.noreply.github.com>
2025-01-07 03:22:42 -0500

    chore: deno finally supports globs, so we can get modules script

--- .scripts/modules.ts
+++
@@ -1,7 +0,0 @@
-// Since the Deno CLI does not support globs yet, we are using this script to
-// traverse the codebase.
-
-import '../cli/mod.ts';
-import '../engine/mod.ts';
-import '../util/mod.ts';
-import '../worker/mod.ts';
