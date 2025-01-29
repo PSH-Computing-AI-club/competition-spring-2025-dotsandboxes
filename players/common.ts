24ee6e8393e9ad259f0065502b359d4f9641496f
oridim <125223080+oridim@users.noreply.github.com>
2025-01-29 18:49:51 -0500

    refactor: moved adjacent lookup function to commons for another new sample player

--- players/common.ts
+++ players/common.ts
@@ -1,5 +1,27 @@
 // Common utility functions are found here.
 
+const { SLOT_KIND } = Engine;
+const { board: gameBoard } = Game;
+
+const { grid } = gameBoard;
+
+export function getAdjacentSpacerSlots(
+    boxBoardSlot: Engine.IBoxBoardSlot,
+): Engine.ISpacerBoardSlot[] {
+    const { x, y } = boxBoardSlot;
+
+    return [
+        grid[y - 1][x],
+        grid[y + 1][x],
+        grid[y][x - 1],
+        grid[y][x + 1],
+    ].filter((gameBoardSlot) => {
+        const { slotKind } = gameBoardSlot;
+
+        return slotKind === SLOT_KIND.spacer;
+    });
+}
+
 export function sampleArray<T>(array: T[]): T | null {
     const elementIndex = Math.trunc(Math.random() * array.length);
 
